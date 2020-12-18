681628576a32f8980f1b5c507057c414a9bef0cf
caisijun <1qaz2wsx>
2020-12-18 17:25:06 +0800

    完善 8种基本类型 添加 数据结构 导论

---
+++ src/main/java/org/cai/structure/baseStruct/Introduction.java
@@ -0,0 +1,26 @@
+package org.cai.structure.baseStruct;
+
+/**
+ * 数据结构 导论
+ * @Author: caisijun
+ * @Date: 2020/12/18 0018
+ */
+public class Introduction {
+    /*
+     *  （一）数据结构的本质：组织数据的形式。就是如何把 0和1 如何存储在内存中。
+     *  （二）具体可以分为：线性结构 和 非线性结构
+     *  线性结构：数组（Array）、栈（Stack）、队列（Queue）、
+     *  非线性结构：哈希（Hash）、图（Graph）、树（Tree）、链表（Link List）、堆（Heep）
+     *  共八种
+     *  （三）数据结构设计的目的：为了更好地服务算法。
+     *  （四）数据结构基本功能
+     *  a.新增
+     *  b.修改
+     *  c.删除
+     *  d.检索
+     *  （五）设计时需要考虑的方面：
+     *  1.变动和检索的侧重点
+     *  2.是否包含null
+     *
+     */
+}
--- src/main/java/org/cai/structure/baseType/BaseType8.java
+++ src/main/java/org/cai/structure/baseType/BaseType8.java
@@ -1,4 +1,4 @@
-package org.cai.structure.base;
+package org.cai.structure.baseType;
 
 import org.junit.Test;
 
@@ -13,6 +13,8 @@ public class BaseType8 {
     @Test
     public void baseType() {
         /**
+         * 导论
+         * 基本类型是组成bit(位)的方式，就是组织 0和1 的形式。
          * TODO 等价关系
          * 8位（bit）== 1字节(byte)
          * 1024字节 == 1kb
@@ -21,80 +23,77 @@ public class BaseType8 {
          * 1024GB == 1TB
          * ...
          * TODO bit存储的值 有且只能是 0 和 1
-         * TODO 1 bit =0 或者 1 8个位 = 255
-         * TODO 列如（eg.）：（二进制 11111111 ）= （十进制 255）,,,,注意 8个1
+         * TODO 列如（eg.）：（二进制 11111111 ）= （十进制 255）注意： 8个1
          * TODO 因为 byte 存在符号，就是 正数和负数 符号 + - ，所以 byte 的取值范围是（-128 到 128-1）共占用7个bit.;;另外为什么是128-1？因为正整数范围是0~127=128位
          * TODO 二进制第一位是符号位
          *
+         * 数据的类型划分：
+         * 正整数（小->大）：byte->short->int->long
+         * 带小数点数值（小->大）：float->double
+         * 字符型：char
+         * boolean型：本质也是byte，只是被约束只能存储 （0,1） 也就是(false，true)
          */
 
-        /*
-        TODO -> （一）boolean 占用1个字节，就是1*8=8 （bit）
-         */
+
         boolean is_dev = true;
         System.out.println("（一）boolean 占用1个字节，就是1*8=8 （bit）");
-        /*
-        TODO -> （二） byte 占用1个字节，就是 1*8=8 （bit）
-         */
+
         byte byte_min = -128;
         byte byte_max = 127;
         final String seven1 = "1111111";
         System.out.println("（二） byte 占用1个字节，就是 1*8=8 （bit）->byte 最大二进制:" + seven1.length() + " bit" + ",十进制：" + get10Hex(seven1));
 
-        /*
-        TODO -> （三） short 占用2个字节，就是 2*8=16 （bit）
-         */
         short short_max = 32768-1;
         short short_min = -32768;
         final String fifteen1 = "111111111111111";
         System.out.println("（三） short 占用2个字节，就是 2*8=16 （bit）->short 最大二进制:" + fifteen1.length() + " bit" + ",十进制：" + get10Hex(fifteen1));
 
-        /**
-         * TODO -> （四） int 占用4个字节，就是 4*8=32 （bit）
-         * 减去 bit的占位 就是 32-1
-         */
+
         int int_max = 2147483647;
         int int_min = -2147483648;
         final String thirtyOne1 = "1111111111111111111111111111111";
         System.out.println("（四） int 占用4个字节，就是 4*8=32 （bit）->int 最大二进制:" + thirtyOne1.length() + " bit" + ",十进制：" + get10Hex(thirtyOne1));
 
-        /**
-         * TODO -> （五）long 占用8个字节,就是 8*8=64 （bit）
-         * 64-1
-         */
+
         long long_max = 9223372036854775807L;
         long long_min = 9223372036854775807L - 1;
         final String sixtyThree1 = "111111111111111111111111111111111111111111111111111111111111111";
         System.out.println("（五）long 占用8个字节,就是 8*8=64 （bit）->long 最大二进制:" + sixtyThree1.length() + " bit" + ",十进制：" + getLong10Hex(sixtyThree1));
 
-        /**
-         * TODO -> （六）flout 占用4个字节,就是 4*8=32 （bit）
-         * 64-1
-         */
+
         float float_max = 2 ^ 31 - 1;
         float float_min = -2 ^ 31;
         System.out.println("（六）flout 占用4个字节,就是 4*8=32 （bit）");
-        /**
-         * TODO -> （七）double 占用8个字节,就是 8*8=64 （bit）
-         * 64-1
-         */
+
         double double_max = 9223372036854775807L;
         double double_min = 9223372036854775807L - 1;
         System.out.println("（七）double 占用8个字节,就是 8*8=64 （bit）");
-        /**
-         * TODO -> （八）char 占用2个字节,就是 2*8=16 （bit）
-         *
-         */
+
         char char_max = '啊';
         char char_min = 'a';
-        char numberxx = 32770;
+        char number_0 = 0;
+        char number_max = 65535;
+
         //TODO 数值待研究
-        System.out.println("（八）char 占用2个字节,就是 2*8=16 （bit），汉字占用两个字节，就是‘’只能写一个字 。数值待定");
+        System.out.println("（八）char 占用2个字节,就是 2*8=16 （bit），汉字占用两个字节，就是‘’只能写一个字 。数值只能存储0~65535（也就是000000000000000~1111111111111111）");
+        System.out.println("the end");
+        System.out.println("thanks!!");
+
     }
 
+    /**
+     * 二进制字符串 转 int
+     * @param binary
+     * @return
+     */
     int get10Hex(String binary){
         return Integer.valueOf(binary,2);
     }
+    /**
+     * 二进制字符串 转 long
+     * @param binary
+     * @return
+     */
     long getLong10Hex(String binary){
         return Long.valueOf(binary,2);
     }
