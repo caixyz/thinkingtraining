44b751c9d59c02df5e4b15bd75d291e614baa810
caisijun <1qaz2wsx>
2021-01-15 17:25:36 +0800

    新增 注释

--- src/main/java/org/cai/patterns/adapter/test/AdapterTest.java
+++ src/main/java/org/cai/patterns/adapter/test/AdapterTest.java
@@ -4,6 +4,11 @@ import org.cai.patterns.adapter.service.impl.AudioPlayer;
 import org.junit.Test;
 
 /**
+ * 适配器模式 ：就是使接口适配不同 场景
+ * 优点：对外暴露一个接口
+ * 缺点：对内增加了一个适配器对象，不利于项目以后的扩展和维护
+ *
+ *
  * @Author: caisijun
  * @Date: 2021/1/15 0015
  */
